7bbc8ab7c415422d963ae00da6e40be33505368d
Skousen2025 <guca.cph@gmail.com>
2025-05-31 23:03:29 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Skousen App</title>
+      </Head>
+      <main>
+        <h1>Bem-vindo ao App Skousen</h1>
+        <p>Upload de fotos, documentos e rota no Google Maps</p>
+      </main>
+    </div>
+  )
+}
